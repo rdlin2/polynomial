f97455856be38e94f183ed812f67f6935c40e331
Richard Lin <rlin2@bu.edu>
2024-09-11 17:35:13 -0400

    cs 506 part b

--- polynomial.py
+++ polynomial.py
@@ -1,39 +1,53 @@
 class X:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def __repr__(self):
-        return "X"
+	def __repr__(self):
+		return "X"
+	
+	def evaluate (self, i):
+		return i
 
 class Int:
-    def __init__(self, i):
-        self.i = i
-    
-    def __repr__(self):
-        return str(self.i)
+	def __init__(self, i):
+		self.i = i
+	
+	def __repr__(self):
+		return str(self.i)
+	
+	def evaluate(self, i):
+		return self.i
 
 class Add:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-    
-    def __repr__(self):
-        return repr(self.p1) + " + " + repr(self.p2)
+	def __init__(self, p1, p2):
+		self.p1 = p1
+		self.p2 = p2
+
+	def __repr__(self):
+		return repr(self.p1) + " + " + repr(self.p2)
+	
+	def evaluate(self, i):
+		return self.p1.evaluate(i) + self.p2.evaluate(i)
 
 class Mul:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-    
-    def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+	def __init__(self, p1, p2):
+		self.p1 = p1
+		self.p2 = p2
+
+	def __repr__(self):
+		if isinstance(self.p1, Add):
+			if isinstance(self.p2, Add):
+				return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+			return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+		if isinstance(self.p2, Add):
+			return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+		return repr(self.p1) + " * " + repr(self.p2)
+	
+	def evaluate(self, i):
+		return self.p1.evaluate(i) * self.p2.evaluate(i)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(-1))
+
